f9dcd605cdb10b3165182aa5200e7dc9abad911c
wanshan.qiu <wanshan.qiu@lantaiyuan.com>
2018-01-30 11:35:30 +0800

    11

--- match-service/src/main/java/com/lantaiyuan/carpool/match/channel/MatchSubscribeChannel.java
+++ match-service/src/main/java/com/lantaiyuan/carpool/match/channel/MatchSubscribeChannel.java
@@ -1,14 +1,11 @@
 package com.lantaiyuan.carpool.match.channel;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.messaging.SubscribableChannel;
-import org.springframework.stereotype.Component;
 
 /**
  * @author qiuwanshan
  */
-@Component
 public interface MatchSubscribeChannel {
     String SUBSCRIBE = "subscribe";
     /**
