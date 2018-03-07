52d4baecf556a317bd6dff8192b6396f210cfeae
wanshan.qiu <wanshan.qiu@lantaiyuan.com>
2018-03-07 18:46:49 +0800

    添加注释

--- common/src/main/java/com/lantaiyuan/carpool/common/constant/UserStatusEnum.java
+++ common/src/main/java/com/lantaiyuan/carpool/common/constant/UserStatusEnum.java
@@ -12,9 +12,10 @@ public enum UserStatusEnum {
     /**
      * NO_STATUS 空状态
      * MATCH 撮合中状态
-     * BUS_START 已发车状态
+     * ON_BUS 已上车
+     * OFF_BUS 已下车
      */
-    NO_STATUS(201),MATCH(202),BUS_START(203);
+    NO_STATUS(201),MATCH(202),ON_BUS(203),OFF_BUS(204);
     @Getter
     private Integer value;
 
